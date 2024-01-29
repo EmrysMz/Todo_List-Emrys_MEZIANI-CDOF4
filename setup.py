e9d9a22da1c15caae946f11c65c2490d195f6c90
Emrys MEZIANI <emrys.meziani@edu.devinci.fr>
2024-01-29 13:57:17 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+ name='{Todo_List}',
+ version='1.0',
+ author='Emrys MEZIANI',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
